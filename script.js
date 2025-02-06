b88a22bdad0c95b4c6dc67d53470c01a7bb8c7d5
farrel irawan <irawanssfarrel@gmail.com>
2025-02-07 00:13:31 +0700

    membuat fungsi addTodo dengan logika memasukan judul todo dan kapan waktu selesai todo ke variable todoSchema

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
         addTodo();
     });
 });
+
+function addTodo() {
+    const todoTitle = document.getElementById('title').value;
+    const todoFinishDate = document.getElementById('date').value;
+
+    const uniqeId = generateId();
+    const todoSchema = generateTodo(uniqeId, todoTitle, todoFinishDate, false);
+    todos.push(todoSchema);
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
